0b4d0e068481756e20d7f97eb280329ea0da3f45
Yasir Ali <155450120+asad-codingcrafts@users.noreply.github.com>
2024-02-02 23:10:21 +0500

    Sidebar UI logic improved

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
 import colors from "../../styles/colors";
 import styled from "styled-components";
 import { sideBarProcessList } from "../../constants/stubs";
-import { Layout, Menu, Typography } from "antd";
+import { Flex, Layout, Menu, Typography } from "antd";
 
 const { Sider } = Layout;
 const { Text } = Typography;
@@ -14,26 +14,40 @@ const RoundCircle = styled("span")`
   justify-content: center;
   align-items: center;
   background-color: ${colors.Mischka};
+  margin-right: 12px;
 `;
 
-interface NumberIconProps {
+interface SideBarItemProps {
   number: string;
+  title: string;
 }
 
-const NumberIcon = ({ number }: NumberIconProps) => {
+const SideBarItem = ({ number, title }: SideBarItemProps) => {
   return (
-    <RoundCircle>
+    <Flex>
+      <RoundCircle>
+        <Text
+          style={{
+            fontFamily: "Calps",
+            fontSize: "14px",
+            fontWeight: "700",
+            textAlign: "center",
+          }}
+        >
+          {number}
+        </Text>
+      </RoundCircle>
       <Text
         style={{
-          fontFamily: "Calps",
-          fontSize: "14px",
-          fontWeight: "700",
+          fontSize: "16px",
+          fontWeight: "600",
+          lineHeight: "22px",
           textAlign: "center",
         }}
       >
-        {number}
+        {title}
       </Text>
-    </RoundCircle>
+    </Flex>
   );
 };
 
@@ -45,26 +59,12 @@ const Sidebar = () => {
           return (
             <Menu.Item
               key={item.id}
-              icon={
-                <RoundCircle>
-                  <Text style={{ fontWeight: "700" }}>{item.id}</Text>
-                </RoundCircle>
-              }
               style={{
                 padding: "0 0 0 12px",
                 alignItems: "center",
               }}
             >
-              <Text
-                style={{
-                  fontSize: "16px",
-                  fontWeight: "600",
-                  lineHeight: "22px",
-                  textAlign: "center",
-                }}
-              >
-                {item.title}
-              </Text>
+              <SideBarItem number={item.id} title={item.title} />
             </Menu.Item>
           );
         })}
